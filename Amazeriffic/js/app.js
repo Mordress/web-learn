9d3d231b77dbbc19f713b86b05307a4641a07f3b
mordress-win10 <mordress@gmail.com>
2017-07-19 18:19:21 +0300

    before js ref-1

--- Amazeriffic/js/app.js
+++ Amazeriffic/js/app.js
@@ -1,37 +1,22 @@
 $(function() {
     "use strict";
-    $(".tabs a:nth-child(1)").on("click", function () {
-        //делаем все вкладки неактивными
-        $(".tabs span").removeClass("active");
-        //делаем активной первую вкладку
-        $(".tabs a:nth-child(1)").addClass("active");
-        //очищаем основное содержание, чтобы переопределить его
-        $("main .content").empty();
-        //Возвращается false, так как мы не переходим по ссылке
-        return false;
-    });
+    tabActivation(1);
+    tabActivation(2);
+    tabActivation(3);
 
-    $(".tabs a:nth-child(2)").on("click", function () {
-        //делаем все вкладки неактивными
-        $(".tabs span").removeClass("active");
-        //делаем активной первую вкладку
-        $(".tabs a:nth-child(2)").addClass("active");
-        //очищаем основное содержание, чтобы переопределить его
-        $("main .content").empty();
-        //Возвращается false, так как мы не переходим по ссылке
-        return false;
-    });
-
-    $(".tabs a:nth-child(3)").on("click", function () {
-        //делаем все вкладки неактивными
-        $(".tabs span").removeClass("active");
-        //делаем активной первую вкладку
-        $(".tabs a:nth-child(3)").addClass("active");
-        //очищаем основное содержание, чтобы переопределить его
-        $("main .content").empty();
-        //Возвращается false, так как мы не переходим по ссылке
-        return false;
-    });
     console.log("Hello world!");
 });
+
+var tabActivation = function (tabNumber) {
+    $(".tabs a:nth-child(tabNumber)").on("click", function () {
+        //делаем все вкладки неактивными
+        $(".tabs span").removeClass("active");
+        //делаем активной tabNumber-ную вкладку
+        $(".tabs a:nth-child(tabNumber)").addClass("active");
+        //очищаем основное содержание, чтобы переопределить его
+        $("main .content").empty();
+        //Возвращается false, так как мы не переходим по ссылке
+        return false;
+    });
+};
 
