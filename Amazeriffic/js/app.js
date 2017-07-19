cdd9ac91cd3662dc9d374310a04b791e460e769c
mordress-win10 <mordress@gmail.com>
2017-07-19 18:25:14 +0300

    tabs picker added(js)

--- Amazeriffic/js/app.js
+++ Amazeriffic/js/app.js
@@ -1,22 +1,23 @@
 $(function() {
     "use strict";
-    tabActivation(1);
-    tabActivation(2);
-    tabActivation(3);
-
-    console.log("Hello world!");
-});
-
-var tabActivation = function (tabNumber) {
-    $(".tabs a:nth-child(tabNumber)").on("click", function () {
-        //делаем все вкладки неактивными
+    var makeTabActive = function (tabNumber) {
+        //сконструируем селектор из tabNumber
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
         $(".tabs span").removeClass("active");
-        //делаем активной tabNumber-ную вкладку
-        $(".tabs a:nth-child(tabNumber)").addClass("active");
-        //очищаем основное содержание, чтобы переопределить его
-        $("main .content").empty();
-        //Возвращается false, так как мы не переходим по ссылке
+        $(tabSelector).addClass("active");
+    };
+    $(".tabs a:nth-child(1)").on("click", function () {
+        makeTabActive(1);
         return false;
     });
-};
+    $(".tabs a:nth-child(2)").on("click", function () {
+        makeTabActive(2);
+        return false;
+    });
+    $(".tabs a:nth-child(3)").on("click", function () {
+        makeTabActive(3);
+        return false;
+    });
 
+    //console.log("Hello world!");
+});
